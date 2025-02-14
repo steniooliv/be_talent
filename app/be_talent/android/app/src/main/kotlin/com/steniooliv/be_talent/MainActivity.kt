b9a4eac2008ce08db361d0a4d6a1cd4f30ac49b4
Stenio Oliveira <steniooliv@gmail.com>
2025-02-14 17:22:34 -0300

    first commit

---
+++ app/be_talent/android/app/src/main/kotlin/com/steniooliv/be_talent/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.steniooliv.be_talent
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ app/ds_example/android/app/src/main/kotlin/com/steniooliv/ds_example/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.steniooliv.ds_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
