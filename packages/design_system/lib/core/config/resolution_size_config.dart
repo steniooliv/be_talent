ead3e36fbb030554ddc6e5fb494c97c8e27f8091
Stenio Oliveira <steniooliv@gmail.com>
2025-02-19 02:51:26 -0300

    fix(design_system): fix design resolution screen size

--- packages/design_system/lib/core/config/resolution_size_config.dart
+++ packages/design_system/lib/core/config/resolution_size_config.dart
@@ -1,5 +1,5 @@
 /// Reference resolution of Figma prototype screen
 final class ResolutionSizeConfig {
-  static const double width = 390;
-  static const double height = 844;
+  static const double width = 375;
+  static const double height = 921;
 }
