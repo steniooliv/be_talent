d3ba0c3e6fc4faebf9a49870a4c7c4732b0d75d7
Stenio Oliveira <steniooliv@gmail.com>
2025-02-19 02:54:16 -0300

    fix(design_system): fix example list and exports components

--- packages/design_system/lib/exports/examples.dart
+++ packages/design_system/lib/exports/examples.dart
@@ -1,5 +1,7 @@
 import 'package:design_system/design_system.dart';
 import 'package:design_system/src/atoms/image/image_example.dart';
+import 'package:design_system/src/atoms/text/text_example.dart';
+import 'package:design_system/src/molecules/avatar/avatar_example.dart';
 import 'package:design_system/src/molecules/badge/badge_example.dart';
 import 'package:design_system/src/organisms/app_bar/app_bar_example.dart';
 
@@ -8,8 +10,10 @@ class Examples {
 
   static final atoms = <BaseExample>[
     ImageExample(),
+    TextExample(),
   ];
   static final molecules = <BaseExample>[
+    AvatarExample(),
     BadgeExample(),
   ];
   static final organisms = <BaseExample>[
